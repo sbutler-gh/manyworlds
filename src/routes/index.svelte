a6f9d189b6a839a7d830908bcb25ad5f311c9e32
sbutler-gh <sam@ysli.be>
2021-11-22 16:47:19 +0100

    Cleaned up page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -80,37 +80,7 @@ else {
   <input name="email" type="email">
   <button>Submit</button>{#if email_success_1}<p style="color: green;">Success!</p>{/if}
   </form>
-  <hr>
-<p>And you're participating in this pattern in a different way too.  Does this whole way of creating things resonate with you?  Want to share what you're working on with others, and invite them to design it and make it real with you?</p>
-<p><a href="/new_page">Create your own page</a>, just like this one.  You get HTML/CSS which you can fully customize with your vision — and deploy it into a live, working webpage, in a single click.  You can share that page with others, who can engage with your initiative however you recommend (including leaving their email address in a form, like the one above.)  In short, you can communicate your vision, invite others to create it with you, start exploring the possibilities and creating desirable futures together.</p>
-<p>And you're participating in that process right now.  Instead of investing months of effort and risk into creating this tool, I'm choosing to communicate it — and inviting you to create this reality with me.</p>
-<p>Have your own vision, project, change in mind?  <a href="/new_page">Create your page</a>, share it with others, and start making it real — together.</p>
-
-<p>And instead of only seeing possibilities from your limited perspective, you can see many more possibilities as a community.  Because chances are, whatever obstacle you're facing — it's probably not an obstacle at all, and all you need is a different set of eyes or a person with different experiences to help you realize that and move onwards.</p>
-<p>So with all of that in mind, this page is actually demonstrating <strong>two things.</strong></p>
-<p>One, as a <strong>single node</strong> of a bigger picture, is <a href="https://makereal.pages.dev">a tool to help us</a> figure out <strong>what we're trying to do</strong> — <strong>in order to do what</strong> — and <strong>what can help us do it.</strong></p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/SKe-WjWx3NU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<p>For example, maybe you're writing an email to Douglas Rushkoff, because you think that will help you get funding to work on your project, which really would just go to app development so you can start sharing what you have in mind.</p>
-<p>If you think that through, you don't actually need any input from Douglas Rushkoff — nor do you need funding, or software development.  What you're really trying to do is share the vision you have in mind with people.  And there are a lot of ways to do that. (More to come in two paragraphs).</p>
-<p>The vision of <a href="https://makereal.pages.dev">this tool</a> is sort of like a crowdsourced Stack Overflow, for helping people and ourselves figure out what we're trying to do.  It doesn't only need to be used for world-changing projects.  For example, one of the first use cases was helping my partner think through why she wanted a car — and get down to the very specific "in order to", of "have a convenient way to go on weekend trips."  That was her job-to-be-done — not driving to work, not going to the grocery store, not taking kids to soccer practice.  Just the weekend car trips.  And once you have that clarity, it can help you consider a topic in new ways.</p>
-<p>Interested?  Want to explore the possibilities and help bring that to life?</p>
-<p>Enter your email, and you'll be part of the things to come.</p>
-<!-- <form id="1" on:submit|preventDefault={addEmail}>
-<input name="email" type="email">
-<button>Submit</button>{#if email_success_1}<p style="color: green;">Success!</p>{/if}
-</form> -->
-<p>The second thing this page is demonstrating, is a <strong>single node of a bigger picture</strong> — a picture where any project/vision, can start with a page as simple as this, so initiatiors can share those ideas and visions with others, expand the possibility space, and create desired future states together.</p>
-<p>So if you have something in mind, spin up your own page to tell people about it.</p>
-<p>You'll get to edit all the page content yourself, include custom HTML/CSS/JS, own the code — and it comes pre-built with a email form like the one above, so people can sign-up to get updates about your project, and you can do things with those emails like invite them to a Slack/Discord community for further steps.</p>
-<p><em>(NOTE: When making your own page, the pre-built email form functionality is still in development)</em></p>
-<p>Interested in <em>that</em> big picture project?  Want to explore the possibilities and help bring it to life as well?</p>
-<p>Enter your email, like you did above.</p>
-<form id="2" on:submit|preventDefault={addEmail}>
-<input name="email" type="email">
-<button>Submit</button>{#if email_success_2}<p style="color: green;">Success!</p>{/if}
-</form>
-<p>This is how it would work on any given page (at least, to start).</p>
-<p>Want to bring your project to life?  Share your vision, invite others to take part, include a form so they can leave their contact emails.  And whoever's interested, start building it together.</p>
+  <!-- <hr> -->
 </div>
 <style>
     .main{max-width:650px;margin:40px auto;padding:0 10px;font:18px/1.5 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";color:#444}h1,h2,h3{line-height:1.2}@media (prefers-color-scheme: dark){body{color:#ccc;background:black}a:link{color:#5bf}a:visited{color:#ccf}}
